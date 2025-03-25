fc903c93e8e2adb6a0ccd70576f3c95cbc6aa75f
ghetiaalish2602@gmail.com <ghetiaalish2602@gmail.com>
2025-03-25 09:08:14 +0530

    initial commit 25-03-25

--- custom-addons/spacex_website/models/spacex_mission.py
+++ custom-addons/spacex_website/models/spacex_mission.py
@@ -3,10 +3,11 @@ from odoo import models, fields,api
 class SpaceXMission(models.Model):
     _name = 'spacex.mission'
     _description = 'SpaceX Mission'
-
+    _rec_name = 'name'
+    
+    
     name = fields.Char(string='Mission Name', required=True)
     launch_date = fields.Date(string='Launch Date')
     success = fields.Boolean(string='Mission Success')
     rocket_id = fields.Many2one('spacex.rocket', string='Rocket Used')
     Mission_leader = fields.Char(string='Leader Name', required=True)
-    
